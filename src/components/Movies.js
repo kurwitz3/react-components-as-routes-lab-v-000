013d078653616bc972a11960b14f2b0168ebc931
kurwitz3 <kurwitz3@aol.com>
2021-06-04 21:36:52 -0400

    commit tree f1c3ac5eace322f73b577e546d1a8dfa495db2e8

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,21 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+       <h1>Movies Page</h1>
+       {movies.map(movie =>{
+         return(
+           <div>
+             <h2>{movie.title}</h2>
+             <p>TIME:  {movie.time}</p>
+             <ul>
+               {movie.genres.map(genre =>{
+                 return <li>{genre}</li>
+               })}
+             </ul>
+             <p>METASCORE:  {movie.metascore}</p>
+           </div>
+         )
+       })}
     </div>
   );
 };
